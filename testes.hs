61e143574384242aee0747803c1b9b4807c3bf27
tuai-offline <tuai-offline@proton.me>
2022-11-18 09:57:37 +0000

    modified:   testes.hs

--- testes.hs
+++ testes.hs
@@ -1,9 +1,4 @@
-isPrime :: Int -> Bool
-isPrime 1 = False
-isPrime x = if numeroDeDivisores x 2 == 0 then True
-            else False
-
-numeroDeDivisores :: Int -> Int -> Int
-numeroDeDivisores x y | y >= x = 0
-                      | mod x y == 0 = 1
-                      | otherwise = numeroDeDivisores x (y+1)
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert x (h:t) = if x <= h then x : (h:t)
+                 else h : insert x t
